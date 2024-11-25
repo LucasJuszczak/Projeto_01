0b851ad8fcb06c19b2f61ab221c0448499e30f55
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-25 19:28:27 -0300

    .

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+
+    /*cria uma sessão ou retorna o atual com base em um identificaor passado 
+    por meio de uma solicitação GET ou POST ou passado por meio de um cookie*/
+    session_start();
+
     define('INCLUDE_PATH', 'http://localhost/Projeto_01/'); // Definir o domínio do site
     
     $autoload = function($class){
