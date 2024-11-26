f18a9b8b7aadefa03954754235c8eae8dff5aa5a
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-25 22:32:12 -0300

    Update Painel.php

--- assets/classes/Painel.php
+++ assets/classes/Painel.php
@@ -7,7 +7,7 @@
 
         public static function logout(){
             session_destroy();
-            header('location: ' .INCLUDE_PATH_PAINEL);
+            header('Location: '.INCLUDE_PATH_PAINEL);
 
             }
     }
