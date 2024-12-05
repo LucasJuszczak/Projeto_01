ee447125add6641db7811f962c998a83f362eb6d
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-12-04 21:32:48 -0300

    .

--- painel/main.php
+++ painel/main.php
@@ -23,12 +23,19 @@ if (isset($_GET['logout'])) {
     <aside>
 
         <div class="box-usuario">
+            <?php if($_SESSION['img'] == ''){ ?>
             <div class="avatar-usuario">
                 <i class="fa-solid fa-user"></i>
             </div><!--avatar usuario-->
+            <?php } else{ ?>
+
+            <div class="imagem-usuario">
+                <img src="<?php echo INCLUDE_PATH_PAINEL; ?>uploads/<?php echo $_SESSION['img']; ?>" alt="">
+            </div>
+            <?php } ?>
 
             <div class="nome-usuario">
-                <h2><?php echo $_SESSION['nome'];?></h2>
+                <h2><?php echo $_SESSION['nome']; ?></h2>
                 <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
             </div><!--nome usuario-->
         </div><!--box usuario-->
