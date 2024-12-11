1401ccd1ed84f5d4336c69c8566675521c9292eb
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-12-11 20:08:48 -0300

    .

--- painel/main.php
+++ painel/main.php
@@ -77,15 +77,16 @@ if (isset($_GET['logout'])) {
         <div class="box-content left w100">
             <h2>
             <i class="fa-solid fa-house"></i> Painel de controle - <?php echo $nomeEmpresa?></h2>
-
             <div class="box-metricas">
-                <div class="metricas-single">
+                <div class="box-metricas-single">
                     <h2>Usuarios online</h2>
                     <p>10</p>
-                    <div class="metricas-single">
+                </div>
+                    <div class="box-metricas-single">
                     <h2>Visitas hoje</h2>
                     <p>20</p>
-                    <div class="metricas-single">
+                </div>
+                    <div class="box-metricas-single">
                     <h2>Visitas totais</h2>
                     <p>30</p>
                 </div>
