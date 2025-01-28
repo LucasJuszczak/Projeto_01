42e57b24debab376e7d45ba10c01495b8d80a46a
Leandro Balaban <126489808+leandro1027@users.noreply.github.com>
2025-01-27 21:01:16 -0300

    .

--- painel/pages/editar-usuario.php
+++ painel/pages/editar-usuario.php
@@ -1,22 +1,29 @@
-<div class="box-content"></div>
+<div class="box-content">
 
 <h2><i class ="fas ga-edit"></i> Editar usuário</h2>
 
-<form method ="post">
+<form method ="post"enctype="multipart/form-data">
+    <?php
+        if(isset($_POST['acao'])){
+            Painel::messageToUser('sucesso', 'Atualizado com sucesso!');
+        }
+        ?>
     <div class="form-group">
         <label for="nome">Nome:</label>
-        <input type="text" name="Nome" required>
+        <input type="text" name="Nome" required value="<?php echo $_SESSION['nome']; ?>">
     </div>
     <div class="form-group">
         <label for="senha">Senha:</label>
-        <input type="password" name="password" required>
+        <input type="password" name="password" required value="<?php echo $_SESSION['password']; ?>">
     </div>
     <div class="form-group">
         <label for="imagem">Imagem:</label>
         <input type="file" name="imagem">
+        <input type="hidden" name="imagem_atual" value ="<?php echo $_SESSION['img']; ?>">>
     </div>
     <div class="form-group">
         <input type="submit" name="acao" value="Atualizar">
+    </div>
     </div>
     
 </form>
