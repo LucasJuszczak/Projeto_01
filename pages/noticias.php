31543ffdc3f0d9c54f30b2a7d30628854cd6aace
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2025-02-05 21:48:27 -0300

    Update 13

--- pages/noticias.php
+++ pages/noticias.php
@@ -37,14 +37,44 @@
                     </div>
                     <?php echo $infoSite['nome_autor']; ?>
                     <?php echo $infoSite['descricao']; ?>
-                </div><!--text-center-->
-            </div><!--box-content-sidebar-->
-        </div><!--sidebar-->
+                </div>
+                <!--text-center-->
+            </div>
+            <!--box-content-sidebar-->
+        </div>
+        <!--sidebar-->
 
         <div class="conteudo-portal">
             <div class="header-conteudo-portal">
-                
+                <h2>Visualizando Post em <span>Esportes</span></h2>
             </div>
+            <!--header-conteudo-portal-->
+            <?php for ($i = 0; $i < 21; $i++) {
+
+            ?>
+            <div class="box-single-conteudo">
+                <h2>Conheça as novidades do Esp...</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, dolores eligendi totam amet harum
+                    reiciendis voluptas reprehenderit cupiditate, aperiam nemo porro suscipit omnis dolore? Harum dolor
+                    eveniet repellendus et vero?</p>
+                <a href="<?php echo INCLUDE_PATH; ?>noticias/esportes/nome-do-post">Leia mais</a>
+            </div>
+            <!--box-single-conteudo-->
+            <?php } ?>
         </div>
+        <!--conteudo-portal-->
+        <div class="clear"></div>
+        <div class="conteudo-portal">
+            <div class="paginator">
+                <a href="" class="active-page">1</a>
+                <a href="">2</a>
+                <a href="">3</a>
+                <a href="">4</a>
+            </div>
+            <!--paginator-->
+        </div>
+        <!--conteudo-portal-->
+        <div class="clear"></div>
     </div>
+    <!--center-->
 </section>
